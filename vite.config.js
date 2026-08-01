a8b34359f63c2e761bd9071a02ee272393f9cd90
mistryjibon757 <mistry.jibon.757@gmail.com>
2026-08-01 17:49:11 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/messenger/', // ⬅️ আপনার রিপো নাম বসান। স্ল্যাশ (/) দুই পাশে দিতে ভুলবেন না।
   server: {
     port: 5173,
   },
